81a12eee1d0178b590721d8062b2304007a1b04a
Kevin Graney <nanonet@gmail.com>
2026-05-03 17:39:27 -0400

    Bump versionCode

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.kevingraney.c47"
         minSdk = 28
         targetSdk = 36
-        versionCode = 2
+        versionCode = 3
         versionName = "0.0.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
